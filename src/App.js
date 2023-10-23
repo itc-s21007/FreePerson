e324e7488e8991351acfe6a6cddaca2575cd654b
s21007 <s221007@std.it-college.ac.jp>
2023-10-23 16:40:59 +0900

    デザインの追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./App.css";
+import mozaiku from"./mosaic.png";
 import MovieInfo from "./MovieInfo";
 import AnimeInfo from "./AnimeInfo";
 import Books from "./Books";
 
+
 function App() {
+
     return (
         <div className="App">
-         <Books />
+            <div className="container">
+                <div className="left">
+                    <div className="top-left">
+                        <img src={mozaiku} alt="Image" className="image"/>
+                    </div>
+                    <div className="bottom-left">
+                        <button onClick="buttonClick(1)">映画</button>
+                        <button onClick="buttonClick(2)">書籍</button>
+                        <button onClick="buttonClick(3)">アニメ</button>
+                    </div>
+                </div>
+                <div class="right">
+
+                </div>
+            </div>
         </div>
     );
 }
