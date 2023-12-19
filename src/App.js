f7107bd4cb96057440818714737083aeb25e8591
s21007 <s221007@std.it-college.ac.jp>
2023-12-19 15:50:27 +0900

    色々追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,86 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
-import Booksdb from "./acquisition/Booksdb";
 import non from "./noimage.jpeg"
-import Model from "./Model";
+
 
 
 
 function App() {
+
+    const [book,setbook] = useState([]);
+    const [movie, setmovie] = useState([])
+
+    const [show, setshow] = useState(false)
+    const [showdata, setshowdata] = useState([])
+
+    const alldata = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
+
+
+    useEffect(() => {
+        // 漫画と小説データベースのURL
+        // 映画のデータベースのURL
+        const bookUrl = `http://localhost:8080/book`
+        const movieUrl = `http://localhost:8080/movie`
+
+        fetch(bookUrl, {
+            mode : "cors"
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                const bookData = data.data || [];
+                setbook(bookData);
+            })
+            .catch((error) => {
+                console.error('データ取得に失敗しました', error)
+            })
+
+
+        fetch(movieUrl, {
+            mode : "cors"
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                const movieData = data.data || [];
+                setmovie(movieData);
+            })
+            .catch((error) => {
+                console.error('データ取得に失敗しました',error)
+            })
+
+    }, []);
+
+
+
+
+    const all = () => {
+        const data = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
+        book.forEach((books,i) => {
+            data[i % 13].push(books)
+        })
+        movie.forEach((movies,i) => {
+            data[i % 13].push(movies)
+        })
+        setshowdata(data)
+    }
+
+
+
+    const bookdata = () => {
+        const data = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
+        book.forEach((books,i) => {
+            data[i % 13].push(books)
+        })
+        setshowdata(data)
+    }
+
+    const movidata = () => {
+        const data = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
+        movie.forEach((movies,i) => {
+            data[i % 13].push(movies)
+        })
+        setshowdata(data)
+    }
+
 
 
     return (
@@ -15,18 +89,48 @@ function App() {
                 <div className="left">
                     <div className="top-left">
                         <div className="center">
-                            <Model/>
+                            {showdata.map((datas) => (
+                            <>
+                                <div className="CSSMarquee">
+                                    <p>{datas.map((data) => (
+                                        <span onClick={() => setshow(data)}>
+                                            {data.title + "　　　　"}
+                                        </span>))}
+                                    </p>
+
+                                </div>
+
+                                {
+                                    show ?
+                                        <div className="overlay">
+                                            <div className="content">
+                                                {/*<a href="" className="btn btn-gradient" onClick={()=>setshow(false)}><span>Close</span></a>*/}
+                                                <h1 className={"font"}>{show.title ? show.title : "Non Title"}</h1>
+                                                <img className={"img"} src={show.poster ? show.poster : non} height={400} width={400} alt={""}/>
+                                                <h3 className={"author"}>{show.author ? show.author : "情報がありません"}</h3>
+                                                <button className={"button"} onClick={() => setshow(false)}>閉じる</button>
+                                            </div>
+                                        </div>
+                                        : ""
+
+                                }
+
+                            </>
+                            ))}
                         </div>
                     </div>
                     <div className="button-left">
                         <h1 className="lettercolor">暇人 ”暇を楽しみに変える”</h1>
-                        <button className="button" onClick="buttonClick(1)">映画</button>
-                        <button className="button" onClick="buttonClick(2)">書籍</button>
+                        <button className="button" onClick={movidata}>映画</button>
+                        <button className="button" onClick={bookdata}>書籍</button>
+                        <button className="button" onClick={all}>全部</button>
 
                     </div>
                 </div>
                 <div class="right">
-                    <img className={non}/>
+                    <a href="http://asa0325.ddo.jp">
+                        <img className="asa" src="./asa.png" alt="ad"/>
+                    </a>
                 </div>
 
             </div>
--- src/Model.js
+++ src/Model.js
@@ -1,86 +1,94 @@
 import React, {useEffect, useState} from "react";
 import non from "./noimage.jpeg"
-import Booksdb from "./acquisition/Booksdb";
-import Marquee from "react-fast-marquee";
-import books from "./Books";
+
 
 
 function Model() {
-    const [book,setbook] = useState([]);
-    const [movie, setmovie] = useState([])
-
-    useEffect(() => {
-        // 漫画と小説データベースのURL
-        // 映画のデータベースのURL
-        const bookUrl = `http://localhost:8080/book`
-        const movieUrl = `http://localhost:8080/movie`
-
-        fetch(bookUrl, {
-            mode : "cors"
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const bookData = data.data || [];
-                setbook(bookData);
-            })
-            .catch((error) => {
-                console.error('データ取得に失敗しました', error)
-            })
-
-
-        fetch(movieUrl, {
-            mode : "cors"
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const movieData = data.data || [];
-                setmovie(movieData);
-            })
-            .catch((error) => {
-                console.error('データ取得に失敗しました',error)
-            })
-
-    }, []);
-
-    const [show, setshow] = useState(false)
-
-
-    const showdata = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    book.forEach((books,i) => {
-        showdata[i % 13].push(books)
-    })
-    movie.forEach((movies,i) => {
-        showdata[i % 13].push(movies)
-    })
-
-    return showdata.map((datas) => (
-        <>
-            <div className="CSSMarquee">
-                <p>{datas.map((data) => (
-                    <span onClick={() => setshow(data)}>
-                        {data.title + "　　　　"}
-                    </span>
-                ))}
-                </p>
-            </div>
-
-            {
-                show ?
-                    <div className="overlay">
-                        <div className="content">
-                            {/*<a href="" className="btn btn-gradient" onClick={()=>setshow(false)}><span>Close</span></a>*/}
-                            <h1 className={"font"}>{show.title ? show.title : "Non Title"}</h1>
-                            <img className={"img"} src={show.poster ? show.poster : non} height={400} width={400} alt={""}/>
-                            <h3 className={"author"}>{show.author ? show.author : "情報がありません"}</h3>
-                            <button className={"button"} onClick={() => setshow(false)}>閉じる</button>
-                        </div>
-                    </div>
-                    : ""
-
-            }
-
-        </>
-    ))
+    // const [book,setbook] = useState([]);
+    // const [movie, setmovie] = useState([])
+    //
+    // useEffect(() => {
+    //     // 漫画と小説データベースのURL
+    //     // 映画のデータベースのURL
+    //     const bookUrl = `http://localhost:8080/book`
+    //     const movieUrl = `http://localhost:8080/movie`
+    //
+    //     fetch(bookUrl, {
+    //         mode : "cors"
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             const bookData = data.data || [];
+    //             setbook(bookData);
+    //         })
+    //         .catch((error) => {
+    //             console.error('データ取得に失敗しました', error)
+    //         })
+    //
+    //
+    //     fetch(movieUrl, {
+    //         mode : "cors"
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             const movieData = data.data || [];
+    //             setmovie(movieData);
+    //         })
+    //         .catch((error) => {
+    //             console.error('データ取得に失敗しました',error)
+    //         })
+    //
+    // }, []);
+    //
+    // const [show, setshow] = useState(false)
+    //
+    //
+    // let showdata = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    // const bookdata = () => {
+    //     showdata = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    //     book.forEach((books,i) => {
+    //         showdata[i % 13].push(books)
+    //     })
+    // }
+    //
+    // const movies = () => {
+    //     showdata = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    //     movie.forEach((movies,i) => {
+    //         showdata[i % 13].push(movies)
+    //     })
+    // }
+    //
+    //
+    //
+    // return showdata.map((datas) => (
+    //     <>
+    //         <div className="CSSMarquee">
+    //             <p>{datas.map((data) => (
+    //                 <span onClick={() => setshow(data)}>
+    //                     {data.title + "　　　　"}
+    //                 </span>
+    //             ))}showdata = [[],[],[],[],[],[],[]
+    //             </p>
+    //
+    //         </div>
+    //
+    //         {
+    //             show ?
+    //                 <div className="overlay">
+    //                     <div className="content">
+    //                         {/*<a href="" className="btn btn-gradient" onClick={()=>setshow(false)}><span>Close</span></a>*/}
+    //                         <h1 className={"font"}>{show.title ? show.title : "Non Title"}</h1>
+    //                         <img className={"img"} src={show.poster ? show.poster : non} height={400} width={400} alt={""}/>
+    //                         <h3 className={"author"}>{show.author ? show.author : "情報がありません"}</h3>
+    //                         <button className={"button"} onClick={() => setshow(false)}>閉じる</button>
+    //                     </div>
+    //                 </div>
+    //                 : ""
+    //
+    //         }
+    //
+    //     </>
+    // ))
 }
 
 export default Model;
